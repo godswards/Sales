1537542f8fc478bb6503d53f05c01e5f37dd3f29
godswards <godwinatabelo86@gmail.com>
2021-11-13 22:28:02 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('base')
+
+
+
+@section('content')
+
+<div class="jumbotron hero text-center">
+    <h1>Welcome To My Business</h1>
+</div>
+
+<div class="container">
+<div class="card-deck">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Design</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At necessitatibus perferendis asperiores ut nihil dicta, corporis vitae voluptatum quis sed? Animi eius odit cum ad excepturi? Quibusdam quis doloremque maxime.</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Development</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At necessitatibus perferendis asperiores ut nihil dicta, corporis vitae voluptatum quis sed? Animi eius odit cum ad excepturi? Quibusdam quis doloremque maxime.</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Databases</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At necessitatibus perferendis asperiores ut nihil dicta, corporis vitae voluptatum quis sed? Animi eius odit cum ad excepturi? Quibusdam quis doloremque maxime.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+@stop
